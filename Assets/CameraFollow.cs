580175fe094f9d534588978129d8c4cdf04b8648
Ejae <froggy3dev@gmail.com>
2018-12-30 21:57:15 +0900

    Prototyping scene

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void LateUpdate()
+    {
+        transform.localPosition = new Vector3(player.transform.localPosition.x, 0f, player.transform.localPosition.z); 
+    }
+}
