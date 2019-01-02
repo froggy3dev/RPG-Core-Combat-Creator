d404badb00c0d5c73856849c8a6e2218c66923eb
Ejae <froggy3dev@gmail.com>
2019-01-03 08:34:50 +0900

    15. Using Cursor Affordance

---
+++ Assets/Scripts/CursorAffordance.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour
+{
+    [SerializeField] Texture2D walkCursor = null;
+    [SerializeField] Texture2D targetCursor = null;
+    [SerializeField] Texture2D unknownCursor = null;
+
+    [SerializeField] Vector2 hotspot = new Vector2(0, 0);
+
+    CameraRaycaster cameraRaycaster;
+
+    void Start()
+    {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+    }
+
+    void Update()
+    {
+        switch(cameraRaycaster.layerHit)
+        {
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, hotspot, CursorMode.Auto);
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor(targetCursor, hotspot, CursorMode.Auto);
+                break;
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(unknownCursor, hotspot, CursorMode.Auto);
+                break;
+            default:
+                break;
+        }
+    }
+}
